51361d2a53dbd406e14b9ab32af134304ad574af
Shubham Kumar <shubhamkumar.skv88@gmail.com>
2023-10-19 23:42:21 +0530

    Integrates web-pages against menus.

--- techcode/techcode/src/main/java/com/shubham/springmvc/techcode/config/CommonViewController.java
+++ techcode/techcode/src/main/java/com/shubham/springmvc/techcode/config/CommonViewController.java
@@ -12,5 +12,16 @@ public class CommonViewController implements WebMvcConfigurer {
 		
 		registry.addViewController("/courses").setViewName("courses");
 		registry.addViewController("/about").setViewName("about");
+		registry.addViewController("/notice").setViewName("notice");
+		registry.addViewController("/research").setViewName("research");
+		registry.addViewController("/scholarship").setViewName("scholarship");
+		registry.addViewController("/events").setViewName("events");
+		registry.addViewController("/blog").setViewName("blog");
+		registry.addViewController("/teacher").setViewName("teacher");
+		registry.addViewController("/teacher-single").setViewName("teacher-single");
+		registry.addViewController("/notice-single").setViewName("notice-single");
+		registry.addViewController("/course-single").setViewName("course-single");
+		registry.addViewController("/event-single").setViewName("event-single");
+		registry.addViewController("/blog-single").setViewName("blog-single");
 	}
 }
